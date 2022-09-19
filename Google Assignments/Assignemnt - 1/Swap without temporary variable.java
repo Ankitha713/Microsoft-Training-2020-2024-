62e8193bfcb4703259ab5569fc85ea4929d8dcc6
Ankitha713 <113583030+Ankitha713@users.noreply.github.com>
2022-09-19 12:22:57 +0530

    Create Swap without temporary variable.java

---
+++ Google Assignments/Assignemnt - 1/Swap without temporary variable.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int A=sc.nextInt();
+        int B=sc.nextInt();
+        A=A+B;
+        B=A-B;
+        A=A-B;
+        System.out.println(A+" "+B);
+    }
+}
