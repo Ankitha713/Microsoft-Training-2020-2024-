5cdf5b4293038867ad1bfc3c673787464226dc6c
Ankitha713 <113583030+Ankitha713@users.noreply.github.com>
2022-09-19 12:19:54 +0530

    Create Area of Circle.java

---
+++ Google Assignments/Assignemnt - 1/Area of Circle.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int R=sc.nextInt();
+        System.out.println(3.14*R*R);
+    }
+}
