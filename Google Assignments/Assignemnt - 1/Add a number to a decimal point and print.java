dc789a189cb9b384b4408a221ffdb0413ab5ef19
Ankitha713 <113583030+Ankitha713@users.noreply.github.com>
2022-09-19 18:03:03 +0530

    Create Add a number to a decimal point and print.java

---
+++ Google Assignments/Assignemnt - 1/Add a number to a decimal point and print.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int X=sc.nextInt();
+        float Y=sc.nextFloat(); 
+        System.out.println(X+Y);
+    }
+}
